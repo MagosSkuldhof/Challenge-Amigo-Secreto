32d260652ee07343c9a11fcf5509c6862b2d6fc7
Magos Skuldhof <antonioivn@outlook.es>
2025-03-17 20:03:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = []
+
+//Para esta función tomé el nombre que ya se encuentra en index.html, para no tener que cambiarlo ahí.
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value //Crea una variable para poder usarla en este archivo .js
+
+    if (amigo.trim() === ""){ //Este if verifica que el usuario no intente registrar un nombre vacío.
+        alert("¿Olvidaste algo? Necesitas poner un nombre!");
+        return;
+    }
+
+    listaAmigos.push('amigo'); //Añade el nombr ingresado al array listaAmigos [].
+
+    document.getElementById('amigo').value = ""; //Esta línea hace que la caja donde va el nombre se vacíe, pues le "quita" el valor a la Id 'amigo'.
+}
+
+//
