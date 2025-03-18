0b0e5f3e960817cf5fcd9dd4a8d73f9891aadfe2
Magos Skuldhof <antonioivn@outlook.es>
2025-03-17 21:25:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,22 @@ function agregarAmigo() {
         return;
     }
 
-    listaAmigos.push('amigo'); //Añade el nombr ingresado al array listaAmigos [].
+    listaAmigos.push('amigo'); //Añade el nombre ingresado al array listaAmigos [].
 
     document.getElementById('amigo').value = ""; //Esta línea hace que la caja donde va el nombre se vacíe, pues le "quita" el valor a la Id 'amigo'.
+}
+
+//Esta función será la que habilite a la página mostrar los nombres de la lista
+function mostrarListaAmigos (){
+    let elementoLista = document.getElementById('listaAmigos'); //Usando el ID que ya está en el html, lo asigno a u variable para manejarlo
+
+    elementoLista.innerHTML = ''; //Se elimina cualquier contenido para evitar duplicados
+
+    for (let amigo of listaAmigos){ //Este es el ciclo for que pasará por todos los amigos en la lista y los hará elementos de lista <li>
+        let li = document.createElement('li'); //dejo la variable como li para reacionarlo a listas
+        li.textContent = amigo; //Esto pasa el elemento escrito a la variable
+        elementoLista.appendChild(li); //FInalmente se añade el amigo a la lista en el html
+    }
 }
 
 //
